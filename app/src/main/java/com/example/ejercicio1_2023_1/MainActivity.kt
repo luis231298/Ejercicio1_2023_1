c629760e0c3af7916e7d5cd05e4b8a2b708d8a08
José Luis Luna <pepeluna23@yahoo.com>
2022-11-20 08:56:49 -0600

    DataPicker y validación de fecha por año.

--- app/src/main/java/com/example/ejercicio1_2023_1/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1_2023_1/MainActivity.kt
@@ -1,22 +1,49 @@
 package com.example.ejercicio1_2023_1
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.util.PatternsCompat
 import com.example.ejercicio1_2023_1.databinding.ActivityMainBinding
+import java.time.Instant
 
 class MainActivity : AppCompatActivity() {
 
+    var txtFecha:EditText?=null
+    var btnFecha:ImageButton?=null
+    var dpFecha:DatePicker?=null
+
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        txtFecha = binding.txtFecha
+        btnFecha = binding.btnFecha
+        dpFecha = binding.dpFecha
+
+        txtFecha?.setText(getDataPicker())
+
+        dpFecha?.setOnDateChangedListener {
+            dpFecha,anio,mes,dia ->
+            txtFecha?.setText(getDataPicker())
+            dpFecha?.visibility=View.GONE
+        }
+
+
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun click(view: View){
 
         if (binding.ETNombre.text.isNotEmpty()){
@@ -47,6 +74,12 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity,"Campo de correo vacio",Toast.LENGTH_SHORT).show()
         }
 
+        if (Fecha()){
+            Toast.makeText(this@MainActivity,"Fecha Correcta", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this@MainActivity,"Fecha Incorrecta", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     //funcion encargada de comprobar los valores del no. de cuenta
@@ -70,6 +103,42 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, "El correo no tiene un formato valido",Toast.LENGTH_SHORT).show()
             return false
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun Fecha(): Boolean{
+        val horaActual = Instant.now().toString()
+        val delimit = "T"
+        val delim = "-"
+
+
+
+        val time2 = horaActual.split(delimit)
+        val fecha2 = time2[0].split(delim)
+
+        val fecha1 = txtFecha?.text.toString().split(delim)
+
+        //Toast.makeText(this@MainActivity,fecha1.toString(),Toast.LENGTH_LONG).show()
+
+        return fecha1[2] <= fecha2[0]
+
+
+
+        /*Toast.makeText(this@MainActivity,fecha2.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this@MainActivity,anio,Toast.LENGTH_LONG).show()*/
+
+        //return true
+    }
+
+    fun getDataPicker():String{
+        var dia = dpFecha?.dayOfMonth.toString().padStart(2,'0')
+        var mes = (dpFecha!!.month+1).toString().padStart(2,'0')
+        var anio = dpFecha?.year.toString().padStart(4,'0')
+        return dia+"-"+mes+"-"+anio
+    }
+
+    fun muestraCalendario(view: View){
+        dpFecha?.visibility= View.VISIBLE
     }
 
 }
